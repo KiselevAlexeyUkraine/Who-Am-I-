b55bf70fc39dc49408d69b8f046c73b95602da1f
KiselevAlexey <alekskisliy@gmail.com>
2025-05-30 00:07:26 +0300

    настроил для ключей активацию на экране и починил газовые ловушки

---
+++ Assets/Scripts/Components/Ui/ActiveObjectsController.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+
+namespace Components.Ui
+{
+    public class ActiveObjectsController : MonoBehaviour
+    {
+        [Range(0, 3)]
+        [SerializeField]
+        private int activeCount = 0;
+
+        [Tooltip("Назначить ровно 6 GameObjects")]
+        [SerializeField]
+        private List<GameObject> objects = new(6);
+
+        private void OnEnable()
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+            switch (sceneIndex)
+            {
+                case 2:
+                    SetActiveObjects(0);
+                    break;
+                case 3:
+                    SetActiveObjects(2);
+                    break;
+                case 4:
+                case 5:
+                    SetActiveObjects(4);
+                    break;
+                default:
+                    SetActiveObjects(Mathf.Clamp(activeCount * 2, 0, 6));
+                    break;
+            }
+        }
+
+        private void SetActiveObjects(int totalToActivate)
+        {
+            if (objects == null || objects.Count < 6)
+                return;
+
+            for (int i = 0; i < objects.Count; i++)
+            {
+                if (objects[i] != null)
+                    objects[i].SetActive(i < totalToActivate);
+            }
+        }
+    }
+}
