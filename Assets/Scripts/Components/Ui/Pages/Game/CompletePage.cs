043de465394b2c5bc06314923f3117e308093cc4
KiselevAlexey <alekskisliy@gmail.com>
2025-06-01 20:11:16 +0300

    Создал уровень 6 с рандомом и завершением уровня

--- Assets/Scripts/Components/Ui/Pages/Game/CompletePage.cs
+++ Assets/Scripts/Components/Ui/Pages/Game/CompletePage.cs
@@ -2,42 +2,62 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 using Zenject;
 
 namespace Components.Ui.Pages.Game
 {
-	public class CompletePage : BasePage
-	{
-		[SerializeField] private TMP_Text _killed;
-		[SerializeField] private TMP_Text _time;
-		[SerializeField] private Button _nextLevel;
-		[SerializeField] private Button _restart;
-		[SerializeField] private Button _menu;
+    public class CompletePage : BasePage
+    {
+        [SerializeField] private TMP_Text _killed;
+        //[SerializeField] private TMP_Text _time;
+        [SerializeField] private Button _nextLevel;
+        [SerializeField] private Button _restart;
+        [SerializeField] private Button _menu;
 
-		[Inject]
-		private void Construct(PauseService pauseService, PauseSwitcher pauseSwitcher)
-		{
-			OnOpen += () =>
-			{
-				pauseService.Pause();
-				pauseSwitcher.enabled = false;
-			};
-		}
+        [Inject]
+        private void Construct(PauseService pauseService, PauseSwitcher pauseSwitcher)
+        {
+            OnOpen += () =>
+            {
+                pauseService.Pause();
+                pauseSwitcher.enabled = false;
+            };
+        }
 
-		private void Awake()
-		{
-			_killed.text = $"0";
-			_time.text = $"0";
-			_nextLevel.onClick.AddListener(() => { PageSwitcher.Open(PageName.NextLevel).Forget(); });
-			_restart.onClick.AddListener(() => { PageSwitcher.Open(PageName.GameRestart).Forget(); });
-			_menu.onClick.AddListener(() => { PageSwitcher.Open(PageName.GameExit).Forget(); });
-		}
+        private void Awake()
+        {
+            //_killed.text = $"0";
+            //_time.text = $"0";
 
-		private void OnDestroy()
-		{
-			_nextLevel.onClick.RemoveAllListeners();
-			_restart.onClick.RemoveAllListeners();
-			_menu.onClick.RemoveAllListeners();
-		}
-	}
+            _nextLevel.onClick.AddListener(() => { PageSwitcher.Open(PageName.NextLevel).Forget(); });
+            _restart.onClick.AddListener(() => { PageSwitcher.Open(PageName.GameRestart).Forget(); });
+            _menu.onClick.AddListener(() => { PageSwitcher.Open(PageName.GameExit).Forget(); });
+
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int totalScenes = SceneManager.sceneCountInBuildSettings;
+
+            bool isLastLevel = currentSceneIndex >= totalScenes - 1;
+
+            if (isLastLevel)
+            {
+                _killed.text = "Молодец, ты прошёл игру!";
+                _nextLevel.interactable = false;
+                _nextLevel.gameObject.SetActive(false);
+            }
+            else
+            {
+                _killed.text = "Уровень пройден!";
+                _nextLevel.interactable = true;
+                _nextLevel.gameObject.SetActive(true);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _nextLevel.onClick.RemoveAllListeners();
+            _restart.onClick.RemoveAllListeners();
+            _menu.onClick.RemoveAllListeners();
+        }
+    }
 }
