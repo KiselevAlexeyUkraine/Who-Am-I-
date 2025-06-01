944a3185bcfb104aba211f52e1d8d37696fd4d21
KiselevAlexey <alekskisliy@gmail.com>
2025-06-01 22:05:04 +0300

    Bukd fix добавление и фик некоторых багов
    Who am I v.0.2.1

--- Assets/Scripts/Components/PauseSwitcher.cs
+++ Assets/Scripts/Components/PauseSwitcher.cs
@@ -5,30 +5,35 @@ using Zenject;
 
 public class PauseSwitcher : MonoBehaviour
 {
-	private InputService _input;
-	private PageSwitcher _pageSwitcher;
-	private PauseService _pause;
+    private InputService _input;
+    private PageSwitcher _pageSwitcher;
+    private PauseService _pause;
 
-	[Inject]
-	private void Construct(InputService inputService, PageSwitcher pageSwitcher, PauseService pauseService)
-	{
-		_input = inputService;
-		_pageSwitcher = pageSwitcher;
-		_pause = pauseService;
-	}
+    private float _escapeCooldown = 0.2f;
+    private float _lastEscapeTime;
 
-	private void Update()
-	{
-		if (_input.Escape)
-		{
-			if (_pause.IsPaused)
-			{
-				_pageSwitcher.Open(PageName.Stats).Forget();
-			}
-			else
-			{
-				_pageSwitcher.Open(PageName.Pause).Forget();
-			}
-		}
-	}
+    [Inject]
+    private void Construct(InputService inputService, PageSwitcher pageSwitcher, PauseService pauseService)
+    {
+        _input = inputService;
+        _pageSwitcher = pageSwitcher;
+        _pause = pauseService;
+    }
+
+    private void Update()
+    {
+        if (_input.Escape && Time.unscaledTime - _lastEscapeTime >= _escapeCooldown)
+        {
+            _lastEscapeTime = Time.unscaledTime;
+
+            if (_pause.IsPaused)
+            {
+                _pageSwitcher.Open(PageName.Stats).Forget();
+            }
+            else
+            {
+                _pageSwitcher.Open(PageName.Pause).Forget();
+            }
+        }
+    }
 }
